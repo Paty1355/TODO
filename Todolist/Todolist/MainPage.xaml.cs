419c0282f896537aa518f609e1e66812714b4c93
Paty1355 <94170413+Paty1355@users.noreply.github.com>
2024-03-05 21:49:43 +0100

    Dodanie plikow

---
+++ Todolist/Todolist/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Todolist
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<string> items;
+        public MainPage()
+        {
+            InitializeComponent();
+            items = new ObservableCollection<string>()
+            {"Zakupy: chleb, masło, ser","Do zrobienia: obiad, umyć podłogi", "weekend: kino, spacer z psem"};
+            lista.ItemsSource = items;
+        }
+      
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string text = tresc.Text;
+            items.Add(text);
+            tresc.Text = "";
+        }
+    }
+}
